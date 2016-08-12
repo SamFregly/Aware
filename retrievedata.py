96d79307e15e56dadfa32cef89f45fdb104689ee
SamFregly <samfregly@gmail.com>
2016-08-12 11:43:25 -0400

    Add files via upload

--- retrievedata.py
+++ retrievedata.py
@@ -5,10 +5,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-access_token ="3648600621-Qr0HWRdUZ4GP7bwk6nzbIFmdriflsgNcqR22gS8"
-access_token_secret ="5C9QBwYz8HSw3EhjarMVl0oj4TooAbugkgrjNA4MSCYWc"
-consumer_key ="Pz9OLoKLvyr496hZOfS0S5p0l"
-consumer_secret ="Vx29mWbIACRRqeTPuBSNAvDA9poWUFy37MnfurpZ19nsCqvvlJ"
+access_token =""
+access_token_secret =""
+consumer_key =""
+consumer_secret =""
 
 
 #This is a basic listener that just prints received tweets to stdout.
